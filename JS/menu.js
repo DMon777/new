b3da11b27f929ed422341a8197bcdae588b8fb1e
DMon777 <bessalov88@mail.ru>
2016-06-13 22:27:49 +0300

    сделал выпадающее меню

--- JS/menu.js
+++ JS/menu.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+    $(".main_menu li").hover(function(){
 
-
+            $(this).children("ul").slideDown(200)},
+        function(){
+            $(this).children("ul").slideUp(300)
+        });
+    
 });
+
+
