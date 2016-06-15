4f47a995515ab6a22a77e94afe3ee4937229904d
DMon777 <bessalov88@mail.ru>
2016-06-15 19:23:38 +0300

    Доделал страницу со статьями и начал регистрацию

---
+++ JS/comments.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $(".add_comment_button").bind('click',function(event){
+        event.preventDefault();
+
+        $(".add_comment_button").css('display','none');
+        $("#text_comment").show(300);
+
+        $("span.x").bind('click',function(){
+            $("#text_comment").hide();
+            $(".add_comment_button").css('display','inline-block');
+        });
+
+
+    });
+
+
+
+    $(".answer_button").bind('click',function(){
+
+        $(this).css('display','none');
+        $(this).next().show(300);
+
+        $(".x_answer").bind('click',function(){
+            $(".answer_form").hide();
+            $(".answer_button").css('display','inline-block');
+        });
+
+    })
+
+});
